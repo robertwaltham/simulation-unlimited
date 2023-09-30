b60ca27ec160b9eaeac376ada98780538f43eeef
Robert Waltham <rob@blockoftext.com>
2023-09-30 11:49:21 -0700

    viewbuilder

--- Simulation Unlimited/SimulationPicker.swift
+++ Simulation Unlimited/SimulationPicker.swift
@@ -12,96 +12,53 @@ struct SimulationPicker: View {
     var body: some View {
         NavigationStack {
             VStack {
-                
-                NavigationLink {
+                simulationTile(content: {
                     BoidsView()
-                } label: {
+                }, sim: {
                     BoidsView()
-                        .frame(width: 500, height: 200)
-                        .overlay(
-                            HStack {
-                                Image(systemName: "bird.circle")
-                                      .foregroundStyle(.white, .white)
-                                      .font(.system(size: 64))
-                                      .padding()
-                                
-                                Text("Boids")
-                                    .padding(EdgeInsets(top: 5, leading: 5, bottom: 5, trailing: 20))
-                                    .font(.largeTitle)
-                                    .foregroundColor(.white)
-                                    
-                            }
-                            .background(.gray.opacity(0.75), in: RoundedRectangle(cornerSize: CGSize(width: 10, height: 10)))
-                        )
-                        .clipShape(RoundedRectangle(cornerSize: CGSize(width: 10, height: 10)))
-                        .padding(0.9) // hack alert
-                        .background(RoundedRectangle(cornerRadius: 10).fill(.blue))
-//                        .border(.blue)
-
-                        
-                        
-
-                }
+                }, text: "Boids", icon: "bird.circle")
                 
-                NavigationLink {
+                simulationTile(content: {
                     SlimeView()
-                } label: {
-//                    RoundedRectangle(cornerSize: CGSize(width: 10, height: 10))
-//                        .stroke(lineWidth: 1.0)
-//                        .overlay(
-//                            HStack {
-//                                Image(systemName: "drop.circle")
-//                                      .foregroundStyle(.teal, .gray)
-//                                      .font(.system(size: 64))
-//                                      .padding()
-//                                
-//                                Text("Slime")
-//                                    .padding()
-//                                    .font(.largeTitle)
-//                                
-//                            })
-//                        .frame(width: 500, height: 200)
+                }, sim: {
                     SlimeView()
-                        .frame(width: 500, height: 200)
-                        .clipShape(RoundedRectangle(cornerSize: CGSize(width: 10, height: 10)))
-                        .overlay(
-                            HStack {
-                                Image(systemName: "drop.circle")
-                                      .foregroundStyle(.white, .white)
-                                      .font(.system(size: 64))
-                                      .padding()
-                                
-                                Text("Slime")
-                                    .padding(EdgeInsets(top: 5, leading: 5, bottom: 5, trailing: 20))
-                                    .font(.largeTitle)
-                                    .foregroundColor(.white)
-                                    
-                            }.background(.gray.opacity(0.75), in: RoundedRectangle(cornerSize: CGSize(width: 10, height: 10)))
-                        )
-                        
-                }
+                }, text: "Slime", icon: "drop.circle")
                 
-                NavigationLink {
+                simulationTile(content: {
                     Text("TODO")
-                } label: {
-                    RoundedRectangle(cornerSize: CGSize(width: 10, height: 10))
-                        .stroke(lineWidth: 1.0)
-                        .overlay(
-                            HStack {
-                                Image(systemName: "sun.dust.circle")
-                                      .foregroundStyle(.teal, .gray)
-                                      .font(.system(size: 64))
-                                      .padding()
-                                
-                                Text("Particle Life")
-                                    .padding()
-                                    .font(.largeTitle)
-                                
-                            })
-                        .frame(width: 500, height: 200)
-                }
-
+                }, sim: {
+                    Rectangle()
+                }, text: "Particle Life", icon: "sun.dust.circle")
+                
             }.navigationTitle("Simulations")
+        }
+    }
+}
+
+extension SimulationPicker {
+    
+    @ViewBuilder
+    func simulationTile<Content, Simulation>(content: @escaping () -> Content, sim: @escaping () -> Simulation, text: String, icon: String) -> some View where Content: View, Simulation: View {
+        NavigationLink {
+            content()
+        } label: {
+            sim()
+                .frame(width: 500, height: 200)
+                .clipShape(RoundedRectangle(cornerSize: CGSize(width: 10, height: 10)))
+                .overlay(
+                    HStack {
+                        Image(systemName: icon)
+                              .foregroundStyle(.white, .white)
+                              .font(.system(size: 64))
+                              .padding()
+                        
+                        Text(text)
+                            .padding(EdgeInsets(top: 5, leading: 5, bottom: 5, trailing: 20))
+                            .font(.largeTitle)
+                            .foregroundColor(.white)
+                            
+                    }.background(.gray.opacity(0.75), in: RoundedRectangle(cornerSize: CGSize(width: 10, height: 10)))
+                )
         }
     }
 }
